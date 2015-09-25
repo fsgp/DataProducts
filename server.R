07f5b6aa7046c0cdddf0e1634fcd869b07669e6c
fsgp <fgonzalezprada@gmail.com>
2015-09-25 20:22:41 -0300

    2

--- server.R
+++ server.R
@@ -1,42 +1,18 @@
-
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 #load packages
 library(shiny)
 library(ggplot2)
 
 #load datasets
 df1 <- read.csv("waits.csv", sep = ",")
-df2 <- read.csv("waits2.csv",sep = ";")
-#waits <- merge(df1,df2, all.x = TRUE, all.y = TRUE)
-#clean unused columns
-#waits$X = NULL
-#waits$AccountID = NULL
-#waits$ClientBatchDate = NULL
-#waits$DayOfWeek = NULL
-#waits$Min = NULL
-#remove intermediate datasets from memory
-#rm(df1)
-#rm(df2)
-
+df2 <- read.csv("waits2.csv",sep = ",")
+df3 <- read.csv("waits3.csv",sep = ",")
 
 shinyServer(function(input, output) {
     
     output$waitsByDay <- renderPlot({
         
-        if (input$InstanceName == "DBSERVER01"){
-            data <- df1
-        }
-        else
-        {
-            data <- df2
-        }
-        
+        data = GetDataFrame(input$InstanceName)
+
         ggplot(data, aes(x = factor(Day), y = Delta)) +
             geom_bar(stat = "identity", width = .5) +
             facet_wrap( ~ WaitType) +
@@ -47,14 +23,8 @@ shinyServer(function(input, output) {
     
     output$waitsByHour <- renderPlot({
         
-        if (input$InstanceName == "DBSERVER01"){
-            data <- df1
-        }
-        else
-        {
-            data <- df2
-        }
-        
+        data = GetDataFrame(input$InstanceName)
+
         ggplot(data, aes(x = factor(Hour), y = Delta)) +
             geom_bar(stat = "identity", width = .5) +
             facet_wrap( ~ WaitType) +
@@ -66,18 +36,26 @@ shinyServer(function(input, output) {
     # customize the length drop-down menu; display 5 rows per page by default
     output$mytable1 <- renderDataTable({
         
-        if (input$InstanceName == "DBSERVER01"){
-            data <- df1
-        }
-        else
-        {
-            data <- df2
-        }
-        
+        data = GetDataFrame(input$InstanceName)
+
         data
     }, options = list(
         lengthMenu = c(12, 20, 30, 50), pageLength = 12
     ))
     
+    GetDataFrame <- function(instanceName)
+    {
+        if (instanceName == "DBSERVER01"){
+            data <- df1
+        }
+        if (instanceName == "DBSERVER02"){
+            data <- df2
+        }
+        if (instanceName == "DBSERVER03"){
+            data <- df3
+        }
+        
+        return(data)
+    }
     
 })
--- ui.R
+++ ui.R
@@ -1,25 +1,20 @@
-
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 
 shinyUI(fluidPage(title = 'Database Waits Times',
                   
                   #Application title
-                  # titlePanel("Database Waits Times - By Hour and Day"),
+                  titlePanel("Database Waits Times - By Hour and Day"),
                   
                   sidebarLayout(
-                      sidebarPanel(selectInput(
+                      sidebarPanel(
+                          HTML("Please select a server from the dropdown. You can switch between different views of the data by using the tabs below.<br><br>"),
+                          selectInput(
                           "InstanceName",
                           "InstanceName:",
-                          c(list("DBSERVER01","DBSERVER02"))
+                          c(list("DBSERVER01","DBSERVER02","DBSERVER03"))
                       ), width = "50%"),
-                      mainPanel(tabsetPanel(
+                      mainPanel(
+                          tabsetPanel(
                           id = 'dataset',
                           tabPanel('Waits by Day', plotOutput("waitsByDay",  width = "100%")),
                           tabPanel('Waits by Hour', plotOutput("waitsByHour",  width = "100%")),
